a707e518779fb99fd39ec4bf0401396cc9823820
Izzywizz <isfandyar.ali@me.com>
2019-02-13 15:55:19 +0000

    Initial commit

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    static public Vector3 POS = Vector3.zero;   // class variable that allows the BOID's access to it location
+
+    [Header("Set in Inspector")]
+    public float radius = 10;
+    public float xPhase = 0.5f;
+    public float yPhase = 0.4f;
+    public float zPhase = 0.1f;
+
+    //FixedUpdated is called once per physics updated (ie 50x/ second)
+    private void FixedUpdate()
+    {
+        Vector3 tPos = Vector3.zero;
+        Vector3 scale = transform.localScale;
+        tPos.x = Mathf.Sin(xPhase * Time.time) * radius * scale.x;
+        tPos.y = Mathf.Sin(yPhase * Time.time) * radius * scale.y;
+        tPos.z = Mathf.Sin(zPhase * Time.time) * radius * scale.z;
+        transform.position = tPos; //remmeber you can't update the postion directly
+        POS = tPos;
+    }
+
+}
---
+++ Assets/Scripts/LookAtAttractor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This makes the main camera follow the atractors movement
+public class LookAtAttractor : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.LookAt(Attractor.POS); //now the main cmaera willl constantly look at the attractor
+    }
+}
