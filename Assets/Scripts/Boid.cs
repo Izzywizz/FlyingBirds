6bbd2ba55252bf08164699fed512e5520412d815
Isfand <izzywhizzzz@gmail.com>
2019-02-20 10:51:36 +0000

    completed project, boids are now flocking together in formation

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -7,10 +7,12 @@ public class Boid : MonoBehaviour
 
     [Header("Set Dynamically")]
     public Rigidbody rigid;
+    private Neighborhood neighborhood;
 
     // Start is called before the first frame update
     void Awake()
     {
+        neighborhood = GetComponent<Neighborhood>();
         rigid = GetComponent<Rigidbody>();
 
         //Set a random intial position within a sphere radius and our spawn radius from origin (0,0,0)
@@ -36,6 +38,91 @@ public class Boid : MonoBehaviour
         }
         TrailRenderer trail = GetComponent<TrailRenderer>();
         trail.material.SetColor("_TintColor", randomColour);
+    }
+
+    //Called once per physics updated \ie (50x/second)
+    private void FixedUpdate()
+    {
+        Vector3 velocity = rigid.velocity;
+        Spawner spawner = Spawner.S;
+
+        //COLLISION AVOIDANCE - Avoid neighbors who are too close
+        Vector3 velAvoid = Vector3.zero;
+        Vector3 tooClosePos = neighborhood.avgClosePos;
+        //if the response is Vector3.zero, then no need to react
+        if (tooClosePos != Vector3.zero)
+        {
+            velAvoid = pos - tooClosePos;
+            velAvoid.Normalize();
+            velAvoid *= spawner.velocity;
+        }
+
+        //Velcoity MATCHING - Try to match velocity with neighbors
+        Vector3 velAlign = neighborhood.avgVel;
+        // only do more if the velAlign is not VEctor.zero
+        if (velAlign != Vector3.zero)
+        {
+            //we're really intereseted in direction, so nomalise the velcoity
+            velAlign.Normalize();
+            // and then set it to the speed we choose
+            velAlign *= spawner.velocity;
+        }
+
+        //FLOCK CENTERING - Move towards the centre of local neighbors;
+        Vector3 velCentre = neighborhood.avgPos;
+        if (velCentre != Vector3.zero)
+        {
+            velCentre -= transform.position;
+            velCentre.Normalize();
+            velCentre *= spawner.velocity;
+        }
+
+
+        //ATTRACTION - move towards the ATTractor
+        //This gives us the distance as to how close the Boid is to the attractor, neeeded to check whether to push or pull it
+        Vector3 delta = Attractor.POS - pos; 
+        //check whether we're attracted or avoiding the attractor
+        bool attracted = (delta.magnitude > spawner.attractPushDist);
+        Vector3 velocityAttract = delta.normalized * spawner.velocity; //This is to ensure that VelcoityAttract is of the same length as velocity var 
+
+        //Apply all the veclocites
+        float floatDeltaTime = Time.fixedDeltaTime;
+        if (velAvoid != Vector3.zero)
+        {
+            velocity = Vector3.Lerp(velocity, velAvoid, spawner.collAvoid * floatDeltaTime);
+        }
+        else
+        {
+            if (velAlign != Vector3.zero)
+            {
+                velocity = Vector3.Lerp(velocity, velAlign, spawner.velMatching * floatDeltaTime);
+            }
+            if (velCentre != Vector3.zero)
+            {
+                velocity = Vector3.Lerp(velocity, velCentre, spawner.flockCentering * floatDeltaTime);
+            }
+            if (velocityAttract != Vector3.zero)
+            {
+                //Both vectors Velocity and VelocityAtrtact are of the same magnitude (length), makaing the interpolation weight the same, rememebr that interpolation is bcasilly guaging/guessing the distance btween two points
+                // if the 3rd parameter is 0 then it heads towards velicty pos and if its 1 then it heads to VelocityAtrtact
+                if (attracted)
+                { //Lerp is used to give a weieghting in which direction the Boid should be pushed/pulled
+                    velocity = Vector3.Lerp(velocity, velocityAttract, spawner.attractPull * floatDeltaTime);
+                }
+                else
+                {
+                    velocity = Vector3.Lerp(velocity, -velocityAttract, spawner.attractPush * floatDeltaTime);
+                }
+            }
+        }
+
+        //Since we've been using vectors of the same magnitude, we need to normalised the veclotity and multiply to get the final velcoity of the Boid
+        //Set velocity to the velocity set on the spanwer singlteon
+        velocity = velocity.normalized * spawner.velocity;
+        //finally assing this to the rigidbody
+        rigid.velocity = velocity;
+        //look in the drrection of the new velocity
+        LookAhead();
     }
 
     //methods
---
+++ Assets/Scripts/Neighborhood.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neighborhood : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public List<Boid> neighbors;
+    private SphereCollider coll;
+
+    private void Start()
+    {
+        //Remmeber this is a Boid GameObject, settings the neighbor hood difference to half allos them to just touch eachother
+        neighbors = new List<Boid>();
+        coll = GetComponent<SphereCollider>();
+        coll.radius = Spawner.S.neighbourDist / 2;
+    }
+
+    private void FixedUpdate()
+    {
+        //if the neighborhood distance changes from the sphere collider changes then update it (PhysX calc)
+        if (coll.radius != Spawner.S.neighbourDist / 2)
+        {
+            coll.radius = Spawner.S.neighbourDist / 2;
+        }
+    }
+
+    //Recall that a trigger is a collider that allows other things to pass throught it
+    //if the things we pass through is a Boid and not currently on our list, we add it
+    private void OnTriggerEnter(Collider other)
+    {
+        Boid b = other.GetComponent<Boid>();
+        if (b != null)
+        {
+            if (neighbors.IndexOf(b) != -1)
+            {
+                neighbors.Add(b);
+            }
+        }
+    }
+
+    //If the boid has gone, we remove it from our list
+    private void OnTriggerExit(Collider other)
+    {
+        Boid b = GetComponent<Boid>();
+        if (b != null)
+        {
+            if (neighbors.IndexOf(b) != -1)
+            {
+                neighbors.Remove(b);
+            }
+        }
+    }
+
+    //calcs all the Boids in the list average postion
+    public Vector3 avgPos
+    {
+        get
+        {
+            Vector3 avg = Vector3.zero;
+            if (neighbors.Count == 0) return avg;
+
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                avg += neighbors[i].pos;
+            }
+            avg /= neighbors.Count;
+
+            return avg;
+        }
+    }
+
+    //calcs the all the boids velocity
+    public Vector3 avgVel
+    {
+        get
+        {
+            Vector3 avg = Vector3.zero;
+            if (neighbors.Count == 0) return avg;
+
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                avg += neighbors[i].rigid.velocity;
+            }
+
+            avg /= neighbors.Count;
+            return avg; 
+        }
+    }
+
+    ///calcs and looks for neighbors that are within the collisionDist (from the spanwer singteon) and averages their postion.
+    public Vector3 avgClosePos
+    {
+        get
+        {
+            Vector3 avg = Vector3.zero;
+            Vector3 delta;
+            int nearCount = 0;
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                delta = neighbors[i].pos - transform.position;
+                if (delta.magnitude <= Spawner.S.collDist)
+                {
+                    avg = neighbors[i].pos;
+                    nearCount += 1;
+                }
+            }
+            //If there were no neighbors too close. return vector 3 zero
+            if (nearCount == 0) return avg;
+
+            //Otherwise, average their lcoations
+            avg /= nearCount;
+            return avg;
+        }
+    }
+}
